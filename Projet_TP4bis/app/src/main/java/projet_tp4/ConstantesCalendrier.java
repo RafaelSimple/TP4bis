d9a1b5c0e0edfd2afa2ce54a3a84f7c6c465a693
RafaelSimple <rafael.beaulieu@gmail.com>
2025-03-24 16:51:09 +0100

    j'ai tout supprimé

--- Projet_TP4bis/app/src/main/java/projet_tp4/ConstantesCalendrier.java
+++
@@ -1,9 +0,0 @@
-package projet_tp4;
-
-public interface ConstantesCalendrier {
-
-    final String [] JOURS_SEMAINE = {"lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"} ;
-
-    final String [] MOIS = {"janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre",",novembre","décembre"};
-
-}
